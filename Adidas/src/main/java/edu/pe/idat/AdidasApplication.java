abc13eb45c9d5922ff8dab273ca7080ce6d76369
JoseR09 <84692089+JoseR09@users.noreply.github.com>
2021-06-16 15:36:55 -0500

    Add files via upload

---
+++ Adidas/src/main/java/edu/pe/idat/AdidasApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.idat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdidasApplication.class, args);
+	}
+
+}
